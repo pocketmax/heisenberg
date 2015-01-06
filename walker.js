77df05adf7a3fd3a37b8187f3a0a3804f7829770
pocketmax <sadsdf@adfsadf.com>
2015-01-05 22:37:25 -0700

    fixes #2 includes sample object

---
+++ walker.js
@@ -0,0 +1,38 @@
+var _ = require('lodash');
+
+var raw = {
+    world: {
+        northAmerica: [{
+            usa: {
+                az: {
+                    phx: 'woo hoo'
+                }
+            },
+            canada: {
+                montreal: {
+                    city: 'woo hoo, in canada'
+                }
+            }
+        }]
+    }
+};
+
+var walker = function(obj, cb){
+
+
+    var looper = function(val, key){
+        if( _.isObject(val) ) {
+            cb(key, val);
+            _.forIn(val, function (myVal, myKey) {
+                looper(myVal, myKey);
+            });
+        } else {
+            cb(key, val);
+        }
+    }
+    looper(obj);
+};
+
+console.log(JSON.stringify(walker(raw, function(key, val){
+    console.log('key: ' + key + ' obj: ' + val);
+}), null, 1));
